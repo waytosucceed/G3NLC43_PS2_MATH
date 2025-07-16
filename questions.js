0c6d3c65b8f2f7e57e3581d5ace01f7ad6bdda43
waytosucceed <154772608+waytosucceed@users.noreply.github.com>
2025-07-16 18:28:49 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -7,7 +7,7 @@ var previousQuestionIndex = null; // Track the previously displayed question
 var topicName = ''; // Variable to store the topic name
 const submitSound = document.getElementById("submit-sound");
 
-const uniqueKey = "Flip, Turn, Slide and Symmetry";
+const uniqueKey = "Symmetry";
 
 // Helper function to save data in local storage under the unique key
 function saveToLocalStorage(key, value) {
